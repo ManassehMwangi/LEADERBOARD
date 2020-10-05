68138dd1607471a5d517e774b2d830dd85913002
ManassehMwangi <manassehmwangi9@gmail.com>
2020-10-05 20:09:38 +0300

    This is the first Change, ignore the previous

---
+++ app/src/main/java/com/gads/leaderboard/ApiUtils.java
@@ -0,0 +1,12 @@
+package com.gads.leaderboard;
+
+public class ApiUtils {
+    private ApiUtils() {}
+
+    public static final String BASE_URL = "https://docs.google.com/forms/d/e/";
+
+    public static APIService getAPIService() {
+
+        return RetrofitClient.getClient(BASE_URL).create(APIService.class);
+    }
+}
---
+++ app/src/main/java/com/gads/leaderboard/Submission.java
@@ -0,0 +1,45 @@
+package com.gads.leaderboard;
+
+public class Submission {
+    String firstName;
+    String secondName;
+    String email;
+    String git;
+
+    public Submission(){
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGit() {
+        return git;
+    }
+
+    public void setGit(String git) {
+        this.git = git;
+    }
+
+
+}
